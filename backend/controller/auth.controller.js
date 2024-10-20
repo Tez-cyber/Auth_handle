f0b18d9d5e6368fcf611c4700f6e88415dc2b26b
Salami Azeez <salamitopeazeez@gmail.com>
2024-10-20 17:53:22 +0100

    Added controllers folder

---
+++ backend/controller/auth.controller.js
@@ -0,0 +1,22 @@
+// ===========================
+
+class App {
+    // ====== SignUp
+    signup = async (req, res) => {
+        res.send("SignUp page")
+    }
+    
+    // ====== SignUp
+    login = async (req, res) => {
+        res.send("SignUp page")
+    }
+    
+    // ====== SignUp
+    logout = async (req, res) => {
+        res.send("SignUp page")
+    }
+    
+}
+
+const newApp = new App
+export default newApp
--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,7 +1,9 @@
 import express from "express"
 const router = express.Router()
 
-router.get("/signup", (req, res) => {
-    res.send("Signup Page")
-})
+import authController from "../controller/auth.controller.js"
+
+router.get("/signup", authController.signup)
+router.get("/login", authController.signup)
+router.get("/logout", authController.signup)
 export default router
