744b7e894360cf7d9d7e5a80d3e9636ca4fc4b09
Salami Azeez <salamitopeazeez@gmail.com>
2024-11-10 18:35:16 +0100

    Imported cors in index.js

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from "express"
 import dotenv from "dotenv"
 import cookieParser from "cookie-parser"
+import cors from "cors"
 import { connectDB } from "./db/connectDB.js"
 
 dotenv.config()
@@ -8,6 +9,10 @@ const app = express()
 
 
 // ================= Middlewares
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true, // set to true so that cookies and reqs can be handled
+}))
 app.use(express.json()) // parse incoming requests: using req.body
 app.use(cookieParser()) // parse incoming cookies
 
