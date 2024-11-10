385b80b2273c5fc73e4943742ac8a5bfbbc6d56a
Salami Azeez <salamitopeazeez@gmail.com>
2024-11-10 18:06:05 +0100

    created authStore file with to handle state management for signup

--- frontend/src/store/authStore.js
+++ frontend/src/store/authStore.js
@@ -1,2 +1,28 @@
 import { create } from 'zustand'
+import axios from "axios"
+
+const API_URL = "http://localhost:9090/api/auth"
+export const useAuthStore = create(set => ({
+    user: null,
+    isAuthenticated: false,
+    error: null,
+    isLoading: false,
+    isCheckingAuth: true,
+
+    signup: async(email, password, name) => {
+        set({ isLoading: true, error: null })
+
+        try {
+            const response = await axios.post(`${API_URL}/signup`, { email, password,name })
+            set({ 
+                user: response.data.user,
+                isAuthenticated: true,
+                isLoading: false
+            }) //This is because when you signup, user response is sent (check authController)
+        }catch (err) {
+            set({ error: err.response.data.message || "Error signing up", isLoading: false})
+            throw err;
+        }
+    }
+}))
 
