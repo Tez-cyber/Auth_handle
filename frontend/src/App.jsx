d6a1beb75bb2c10afe7e70a3f23548cb24664490
Salami Azeez <salamitopeazeez@gmail.com>
2024-11-10 12:56:18 +0100

    Created verifyemail page and added in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { FloatingShape } from "./components/floatingShape"
 import { Route, Routes } from "react-router-dom"
 import { SignupPage } from "./pages/signupPage"
 import { LoginPage } from "./pages/loginPage"
+import { EmailVerificationPage } from "./pages/emailVerificationPage"
 
 function App() {
 
@@ -28,6 +29,7 @@ function App() {
           <Route path="/" element={"Home page"} />
           <Route path="/signup" element={<SignupPage />} />
           <Route path="/login" element={<LoginPage />} />
+          <Route path="/verify-email" element={<EmailVerificationPage />} />
         </Routes>
       </div>
     </>
